b26a0b942dedeed2c93588fbcc99713f06d50f5b
Sean Montague <stmontague1@gmail.com>
2021-12-19 17:38:01 -0800

    initial commit

---
+++ src/main/java/org/stm/simplephonebook/SimplephonebookApplication.java
@@ -0,0 +1,13 @@
+package org.stm.simplephonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplephonebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplephonebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/stm/simplephonebook/SimplephonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stm.simplephonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplephonebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
